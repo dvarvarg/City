716609d562dbef0501c3a98b3b8de3deb060b426
dvarvarg <dvarvarg@gmail.com>
2024-10-26 03:03:21 +0300

    Добавлены слова Широта и Долгота

--- main_Denis_Golikov_video.py
+++ main_Denis_Golikov_video.py
@@ -8,8 +8,7 @@ def get_coordinates(city,key):
         if results:
             lat=round(results[0]['geometry']['lat'],2)
             lon=round(results[0]['geometry']['lng'],2)
-
-            return lat, lon
+            return f'Широта: {lat}, Долгота: {lon}'
         else:
             return 'Город не найден'
     except Exception as e:
@@ -17,7 +16,7 @@ def get_coordinates(city,key):
 
 
 key = '31889b09103942fca06007cc1ed29fe1'
-city='Химки'
+city='Эквадор'
 coordinates=get_coordinates(city,key)
 print(f'Координаты города {city}: {coordinates}')
 
